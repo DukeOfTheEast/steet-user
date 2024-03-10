ab3d6fbedca60d775def8636b4d7c4442b40809e
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-03-10 23:11:28 +0000

    added signup auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+"use client";
+
 import SignUp from "@/components/signup/page";
 import Image from "next/image";
+import { useEffect } from "react";
+import app from "./firebase/config";
 
 export default function Home() {
+  useEffect(() => {
+    // Ensure Firebase is initialized only once
+    if (!app.apps || !app.apps.length) {
+      app;
+    }
+  }, []);
+
   return (
     <main className="">
       <SignUp />
--- src/components/signup/page.jsx
+++ src/components/signup/page.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import SignupImg from "../../images/signup.png";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import auth from "../../app/firebase/config";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const [createUserWithEmailAndPassword] =
+  //   useCreateUserWithEmailAndPassword(auth);
+
+  const handleSignup = async () => {
+    try {
+      const auth = getAuth();
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log({ res });
+      setEmail("");
+      setPassword("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <main>
+    <main className="flex flex-row">
       <div>
         <Image className="" src={SignupImg} priority alt="" />
       </div>
       <div>
         <h3></h3>
         <p></p>
-        <div>
+        <div className="flex flex-col">
           <input type="text" placeholder="Full name" />
-          <input type="email" placeholder="Email" />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input type="text" placeholder="Phone Number" />
-          <input type="text" placeholder="Password" />
-          <button>Sign Up</button>
+          <input
+            type="text"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleSignup} className="bg-black text-white">
+            Sign Up
+          </button>
         </div>
       </div>
     </main>
