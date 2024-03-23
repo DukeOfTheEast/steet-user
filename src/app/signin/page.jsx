016a381fd98750b0b59224a85278c665e85e8d26
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-03-23 18:00:21 +0100

    error

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -5,12 +5,21 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/config";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+// import ReactModal from "react-modal";
+
+// ReactModal.setAppElement("#__next");
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const router = useRouter();
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setIsOpen(true);
+  //   setTimeout(() => setIsOpen(false), 3000);
+  // };
 
   const handleSignIn = async (e) => {
     e.preventDefault();
@@ -22,6 +31,9 @@ const SignIn = () => {
       router.push("/dashboard");
     } catch (error) {
       setError(error.message);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
     }
   };
 
@@ -31,7 +43,11 @@ const SignIn = () => {
         Hello! Welcome back.
       </h2>
       <p className="italic text-slate-100">Enter your information to login.</p>
-      {error && <p>{error}</p>}
+      {error && (
+        <p className="text-red-500 bg-white rounded-md px-3 py-1 absolute top-5">
+          Invalid Email or Password!
+        </p>
+      )}
       <form
         className="flex flex-col gap-5 sm:w-[400px] w-[300] bg-white px-5 py-8 rounded-2xl sm:mb-20"
         onSubmit={handleSignIn}
