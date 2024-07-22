de0139f50e682e4370a9eee0a2aa6984e31a0f78
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-07-22 15:08:07 +0100

    delete

--- src/app/dashboard/home/page.jsx
+++ src/app/dashboard/home/page.jsx
@@ -16,11 +16,13 @@ import {
   addDoc,
   doc,
   updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import PostModal from "@/components/post-modal/page";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
+import { AiOutlineDelete, AiOutlineDownload } from "react-icons/ai";
 
 const Home = () => {
   const { currentUser } = useAuth();
@@ -63,6 +65,32 @@ const Home = () => {
     }
   };
 
+  const handleDelete = async (postId) => {
+    try {
+      await deleteDoc(doc(db, "posts", postId));
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
+    } catch (error) {
+      console.error("Error deleting post: ", error);
+    }
+  };
+
+  const handleDownload = async (imageUrl) => {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "image.jpg"; // You can set a more dynamic name if needed
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading image: ", error);
+    }
+  };
+
   return (
     <div className="sm:flex">
       <Navbar />
@@ -88,15 +116,30 @@ const Home = () => {
                   className="w-full rounded-xl mb-4"
                 />
               )}
-              <div className="flex ml-auto items-center gap-1">
-                <button onClick={() => handleLike(post.id)}>
-                  {post.likes.includes(currentUser.uid) ? (
-                    <FaHeart />
-                  ) : (
-                    <FaRegHeart />
-                  )}{" "}
+              <div className="flex flex-row-reverse items-center justify-between">
+                <div className="flex items-center gap-1">
+                  <button onClick={() => handleLike(post.id)}>
+                    {post.likes.includes(currentUser.uid) ? (
+                      <FaHeart />
+                    ) : (
+                      <FaRegHeart />
+                    )}{" "}
+                  </button>
+                  <p>{post.likes.length}</p>
+                </div>
+                <button
+                  onClick={() => handleDownload(post.imageUrl)}
+                  className=""
+                >
+                  <AiOutlineDownload size={20} />
                 </button>
-                <p>{post.likes.length}</p>
+                <div>
+                  {post.createdBy === currentUser.uid && (
+                    <button onClick={() => handleDelete(post.id)} className="">
+                      <AiOutlineDelete size={20} className="mr-2" />
+                    </button>
+                  )}
+                </div>
               </div>
               <hr className="mb-5" />
             </div>
