7502bee07ab247489e955e4e4660409519164a88
chibikaay <okaforanthony11111@gmail.com>
2024-09-19 15:06:11 +0100

    font and button enlargement, login position change

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -3,23 +3,26 @@ import React from "react";
 const Signup = () => {
   return (
     <div className="bg-signUp-bg bg-no-repeat h-screen w-screen bg-cover flex flex-col items-center pt-32">
-      <div >
-        <h1 className="font-sans font-bold text-xl text-[#000000] ">Create your Meet ‘n’ Style account</h1>
+      <div className="mt-20" >
+        <h1 className="font-sans font-bold text-3xl text-[#000000] ">Create your Meet ‘n’ Style account</h1>
       </div>
       
-        <div>
-        <p className="font-sans text-sm p-5">Already have an account? <span className="text-[#FF5C00]"><a href="">Log in</a></span></p>
-        </div>
-      
-      <div className="items-center">
+      <div className="items-center mt-10">
       <div>
-        <button className="bg-[#FF5C00] py-1 px-20 mb-2 font-sans text-[#FFFFFF] "><a href="">Sign up as a designer</a></button>
+        
+        <button className="bg-[#FF5C00] py-1 px-64  mb-2 font-sans text-[#FFFFFF] ">  Sign up as a designer </button>
+         
+
       </div>
       <div>
-      <button className="bg-[#FF5C00] py-1 px-20 mt-2 text-[#FFFFFF]"><a href="">Sign up as a customer </a></button>
+      <button className="bg-[#FF5C00] py-1 px-64 mt-2 ml-1 text-[#FFFFFF]"><a href="">Sign up as a customer </a></button>
 
       </div>
       </div>
+
+      <div>
+        <p className="font-sans text-sm mt-7 p-5">Already have an account? <span className="text-[#FF5C00]"><a href="">Log in</a></span></p>
+        </div>
       
     </div>
   );
