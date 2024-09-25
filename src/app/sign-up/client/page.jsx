53a5ffb5e9873d069a04ba230a1bac0e09872098
chibikaay <okaforanthony11111@gmail.com>
2024-09-25 16:26:42 +0100

    button text change

--- src/app/sign-up/client/page.jsx
+++ src/app/sign-up/client/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ClientSignUp = () => {
-  return <div>ClientSignUp</div>;
+  return (
+<div className="bg-signUp-bg bg-no-repeat h-screen w-screen bg-cover flex flex-col items-center pt-32">
+
+  
+</div>
+  );
+  
+ 
 };
 
 export default ClientSignUp;
